b5f7d74edd222553a31ea4a4ea1bd5e508bff06c
Thais Canguçu <160797281+Thaiscangucu@users.noreply.github.com>
2025-09-10 17:15:39 -0300

    created xcode project

---
+++ taPago/taPago/taPagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  taPagoApp.swift
+//  taPago
+//
+//  Created by Thais Cangucu on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct taPagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ taPago/taPagoTests/taPagoTests.swift
@@ -0,0 +1,16 @@
+//
+//  taPagoTests.swift
+//  taPagoTests
+//
+//  Created by Thais Cangucu on 10/09/25.
+//
+
+import Testing
+
+struct taPagoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

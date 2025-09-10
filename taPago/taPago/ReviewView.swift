8d584a295c40ad261ddaac87f7b0d4a6605a70b8
hissapinto <isabelahissap@gmail.com>
2025-09-10 17:47:47 -0300

    Inicio do trabalho

---
+++ taPago/taPago/ReviewView.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewView.swift
+//  taPago
+//
+//  Created by Isabela Hissa Pinto on 10/09/25.
+//
+
+import SwiftUI
+
+struct ReviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReviewView()
+}
